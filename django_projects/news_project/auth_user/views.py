e99f08f3f30f88a0f1abfc1a66dae4fd8b7661b1
Azimboyev-F7 <azimboyevfaxriyor.official@gmail.com>
2025-03-11 16:42:11 +0500

    modified

--- django_projects/news_project/auth_user/views.py
+++ django_projects/news_project/auth_user/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.contrib import auth
+from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import login, logout, authenticate
 
 def login_view(request):
 
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You are now logged in")
+            return redirect("/")
+        raise ObjectDoesNotExist("username or password is incorrect")
     context = {}
 
     return render(request, 'auth/login.html', context)
