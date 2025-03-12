944f54cf998baab8e65777ae78e0487288c63a82
Azimboyev-F7 <azimboyevfaxriyor.official@gmail.com>
2025-03-12 12:38:49 +0500

    initial

--- django_projects/news_project/auth_user/forms.py
+++ django_projects/news_project/auth_user/forms.py
@@ -1,1 +1,38 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class MyUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+    def __init__(self, *args, **kwargs):
+        super(MyUserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'name': 'username',
+            'placeholder': 'Enter your Username',
+            'id': 'exampleFormControlUsername',
+        })
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control',
+            'name': 'password1',
+            'placeholder': 'Enter your Password',
+            'id': 'exampleFormControlPassword',
+        })
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control',
+            'name': 'confirm_password',
+            'placeholder': 'Confirm your Password',
+            'id': 'exampleFormControlConfirmPassword',
+        })
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control',
+            'name': 'email',
+            'placeholder': 'Enter your Email',
+            'id': 'exampleFormControlEmail',
+        })
--- django_projects/news_project/auth_user/views.py
+++ django_projects/news_project/auth_user/views.py
@@ -5,6 +5,7 @@ from django.contrib import auth
 from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import login, logout, authenticate
+from .forms import MyUserCreationForm
 
 def login_view(request):
 
@@ -30,7 +31,16 @@ def logout_view(request):
     return render(request, 'auth/logout.html', context)
 
 def register_view(request):
+    form = MyUserCreationForm()
+    if request.method == "POST":
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "You are now registered")
+            return redirect("auth_user:login")
 
-    context = {}
+    context = {
+        'form': form,
+    }
 
     return render(request, 'auth/register.html', context)
