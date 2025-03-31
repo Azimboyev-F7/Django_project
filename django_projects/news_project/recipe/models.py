ff259feb463610737cd86407fd70b425f2091a51
Azimboyev-F7 <azimboyevfaxriyor.official@gmail.com>
2025-03-31 13:45:32 +0500

    intial

--- django_projects/news_project/recipe/models.py
+++ django_projects/news_project/recipe/models.py
@@ -13,7 +13,7 @@ class Tag(models.Model):
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='recipe_images/')
-    slug = models.SlugField(null=True, blank=True)
+    # slug = models.SlugField(null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.SET, default=1)
     description = models.TextField()
     tags = models.ManyToManyField(Tag)
@@ -21,36 +21,34 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 class Ingredient(models.Model):
-    UNITS_CHOICEs = (
+    UNITS_CHOICES = (
         (0, 'Kilogram'),
         (1, 'Gram'),
         (2, 'Litre'),
-        (3, 'Piece'),
+        (3, 'Piece')
     )
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    slug = models.SlugField(null=True, blank=True)
     quantity = models.CharField(max_length=200)
-    unit = models.IntegerField(choices=UNITS_CHOICEs)
+    unit = models.IntegerField(choices=UNITS_CHOICES)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True) 
+    updated_at = models.DateTimeField(auto_now=True)
 
 
+# def recipe_pre_save(sender, instance, *args, **kwargs):
+#     if instance.slug is None:
+#         if Recipe.objects.filter(slug=slugify(instance.title)).exclude(id=instance.id).exists():
+#             import uuid
+#             instance.slug = f"{slugify(instance.title)}-{str(uuid.uuid4()).split('-')[0]}"
 
-def recipe_pre_save(sender, instance, *args, **kwargs):
-    if instance.slug is None:
-        if Recipe.objects.filter(slug=slugify(instance.title)).exclude(id=instance.id).exists():
-            import uuid
-            instance.slug = f"{slugify(instance.title)}-{str(uuid.uuid4()).split('-')[0]}"
-
-pre_save.connect(recipe_pre_save, sender=Recipe)
+# pre_save.connect(recipe_pre_save, sender=Recipe)
 
 
-def ingredient_pre_save(sender, instance, *args, **kwargs):
-    if instance.slug is None:
-        if Ingredient.objects.filter(slug=slugify(instance.name)).exclude(id=instance.id).exists():
-            import uuid
-            instance.slug = f"{slugify(instance.name)}-{str(uuid.uuid4()).split('-')[0]}"
+# def ingredient_pre_save(sender, instance, *args, **kwargs):
+#     if instance.slug is None:
+#         if Ingredient.objects.filter(slug=slugify(instance.name)).exclude(id=instance.id).exists():
+#             import uuid
+#             instance.slug = f"{slugify(instance.name)}-{str(uuid.uuid4()).split('-')[0]}"
 
-pre_save.connect(ingredient_pre_save, sender=Ingredient)
+# pre_save.connect(ingredient_pre_save, sender=Ingredient)
