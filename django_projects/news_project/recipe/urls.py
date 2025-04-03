08e8f440887206eb2121426e585ae3c519815296
Azimboyev-F7 <azimboyevfaxriyor.official@gmail.com>
2025-04-03 16:49:42 +0500

    intial

--- django_projects/news_project/recipe/urls.py
+++ django_projects/news_project/recipe/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from .views import recipe_list, recipe_detail
 
 app_name = "recipe" 
 
 
 urlpatterns = [
+    path("list/", recipe_list, name="list"),
+    path("<slug:slug>", recipe_detail, name='detail'),
+
 ]
--- django_projects/news_project/recipe/views.py
+++ django_projects/news_project/recipe/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Recipe, Ingredient, Tag
+from django.contrib.auth.models import User, AbstractBaseUser, Permission
 # Create your views here.
+
+
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    context = {
+        'recipes': recipes,
+    }
+
+    return render(request, 'recipe/index.html', context)
+
+def recipe_detail(request, slug):
+    recipe = get_object_or_404(Recipe, slug=slug)
+    context = {
+        'recipe': recipe,
+    }
+    return render(request, 'recipe/detail.html', context)
