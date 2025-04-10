58d8e848e2b8670921bba23503680b53062f3ee6
Azimboyev-F7 <azimboyevfaxriyor.official@gmail.com>
2025-04-10 17:08:28 +0500

    initial

--- django_projects/news_project/recipe/urls.py
+++ django_projects/news_project/recipe/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import recipe_list, recipe_detail, recipe_create, recipe_update
+from .views import recipe_list, recipe_detail, recipe_create, recipe_update, recipe_delete
 
 app_name = "recipe" 
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("detail/<slug:slug>", recipe_detail, name='detail'),
     path("create-form/", recipe_create, name="recipe_create"),
     path("update/<slug:slug>", recipe_update, name="recipe_update"),
+    path("delete/<slug:slug>", recipe_delete, name="recipe_delete"),
 ]
--- django_projects/news_project/recipe/views.py
+++ django_projects/news_project/recipe/views.py
@@ -26,7 +26,8 @@ def recipe_detail(request, slug):
     print(is_author)
     context = {
         'recipe': recipe,
-        'ingredients': ingredients
+        'ingredients': ingredients,
+        'is_author': is_author
     }
     return render(request, 'recipe/detail.html', context)
 
@@ -52,6 +53,10 @@ def recipe_create(request):
 
 def recipe_update(request, slug):
     recipe = get_object_or_404(Recipe, slug=slug)
+    if not request.user.is_authenticated:
+        messages.warning(request, "You must be logged in to update a recipe")
+        reverse_url = reverse('auth_user:login') + '?next=' + request.path
+        return redirect(reverse_url)
     form = RecipeForm(instance=recipe)
     if request.method == "POST":
         form = RecipeForm(request.POST, request.FILES, instance=recipe)
@@ -60,9 +65,28 @@ def recipe_update(request, slug):
             recipe.author.id = request.user.id
             recipe.save()
             form.save_m2m()
-            return redirect('recipe:list')
+            reverse_url = reverse('recipe:detail', args=[recipe.slug])
+            return redirect(reverse_url)
     context = {
         'form': form
     }
     return render(request, 'recipe/update.html', context)
 
+
+def recipe_delete(request, slug):
+    recipe = get_object_or_404(Recipe, slug=slug)
+    if not request.user.is_authenticated:
+        messages.warning(request, "You must be logged in to delete a recipe")
+        reverse_url = reverse('auth_user:login') + '?next=' + request.path
+        return redirect(reverse_url)
+    
+    if request.method == "POST":
+        recipe.delete()
+        messages.error(request, f"'{recipe.title}' successfully deleted")
+        return redirect('recipe:list')
+
+    context = {
+        'object': recipe
+    }
+
+    return render(request, 'recipe/delete_recipe.html', context)
